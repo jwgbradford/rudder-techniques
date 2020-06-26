9f04e07419dcc867eb916d52292edba7d0a5cc1b
John Bradford <jwgbradford@gmail.com>
2020-06-26 17:03:42 +0100

    Update rudder-6-1.sh

--- rudder-6-1.sh
+++ rudder-6-1.sh
@@ -7,6 +7,5 @@ wget -P /home/digiadmin/Downloads http://repository.rudder.io/apt/6.1/pool/main/
 sudo gdebi --non-interactive /home/digiadmin/Downloads/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
 
 sudo rm -f /home/digiadmin/Downloads/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
-
-rudder agent inventory
-rudder agent run
+sudo rudder agent reset
+sudo rudder agent run
