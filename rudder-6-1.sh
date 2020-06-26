98a5cb1a97e4576643a453e5b6cafba7696829cb
John Bradford <jwgbradford@gmail.com>
2020-06-26 16:23:14 +0100

    Create rudder-6-1.sh

---
+++ rudder-6-1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+http://repository.rudder.io/apt/6.1/pool/main/r/rudder-agent/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
+
+wget -P /home/digiadmin/Downloads http://repository.rudder.io/apt/6.1/pool/main/r/rudder-agent/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
+
+sudo gdebi --non-interactive /home/digiadmin/Downloads/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
+
+sudo rm -f /home/digiadmin/Downloads/rudder-agent_6.1.0-ubuntu20.04_amd64.deb
+echo '104.248.170.79' > /var/rudder/cfengine-community/policy_server.dat
+
+rudder agent inventory
+rudder agent run
