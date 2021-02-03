0eb70df4883c725c0b443f782ea937bed8dda3b2
John Bradford <jwgbradford@gmail.com>
2021-02-03 15:58:13 +0000

    removed echo commands as they don't show up anyway

--- digi_auto.sh
+++ digi_auto.sh
@@ -1,50 +1,40 @@
+add-apt-repository multiverse
 apt update
-apt full-upgrade -y
 
 #install core dependencies
-apt install -y python3-dev openjdk-11-jdk gdebi
+apt install -y openjdk-11-jdk gdebi
 
 #install apt packages
 apt install -y apt-transport-https python3-pip git
-echo "apt installs finished"
 
 #install vs code by apt
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-echo "microsoft keys added"
 
 apt-get update
 apt-get install code -y
 
 #install pip packages
-pip3 install pygame==2.0.0.dev10
-echo "pip3 installs done"
+#pip3 install pygame==2.0.0.dev10
 
 #install snap packages
 snap install slack --classic
-echo "snap installs done"
 
 #install .deb packages
 wget -P $HOME/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 gdebi $HOME/Downloads/google-chrome-stable_current_amd64.deb --non-interactive
 rm $HOME/Downloads/google-chrome-stable_current_amd64.deb
-echo "Chrome deb installed"
 
 wget -P $HOME/Downloads https://zoom.us/client/latest/zoom_amd64.deb
 gdebi --non-interactive $HOME/Downloads/zoom_amd64.deb
 rm -f $HOME/Downloads/zoom_amd64.deb
-echo "zoom deb installed"
 
 #set desktop favourites
 #gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'libreoffice-writer.desktop', 'google-chrome.desktop']"
 
 #set ufw to default
 ufw enable
-echo "ufw set"
-
-add-apt-repository multiverse
-apt update
 
 #SERIAL=$(sudo dmidecode -t system | grep Serial)
 #set -- $SERIAL
@@ -53,7 +43,6 @@ SERIAL=$(sudo dmidecode -s system-serial-number)
 NEWHOSTNAME='digilocal-'$SERIAL
 #Set hostname
 hostnamectl set-hostname $NEWHOSTNAME
-echo "hostname set"
 
 #install rudder.io agent
 wget --quiet -O- "https://repository.rudder.io/apt/rudder_apt_key.pub" | apt-key add -
@@ -65,20 +54,16 @@ apt-get install rudder-agent -y
 echo '104.248.170.79' > /var/rudder/cfengine-community/policy_server.dat
 rudder agent inventory
 rudder agent run
-echo "rudder running"
 
 #set up default user
 useradd -m DigiHome
 echo 'DigiHome:Digi@Home' | chpasswd
-echo "DigiHome added"
 
 #final update and cleanup
 apt update
 apt full-upgrade -y
 apt autoremove -y
-echo "final updates run"
 
 #check and update firmware
 fwupdmgr refresh
 fwupdmgr update -y
-echo "firmware check and exit"
