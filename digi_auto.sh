5c53af5d24a1620582887a9030057b15b1da22c2
John Bradford <jwgbradford@gmail.com>
2021-02-06 08:17:43 +0000

    update to /run/mydir

--- digi_auto.sh
+++ digi_auto.sh
@@ -6,20 +6,19 @@ apt update
 
 ##install snap packages
 #snap install slack --classic
-mkdir /target/temp-inst
 
 ##install .deb packages
-wget -P /target/temp-inst https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi /target/temp-inst/google-chrome-stable_current_amd64.deb --non-interactive
-rm /target/temp-inst/google-chrome-stable_current_amd64.deb
+wget -P /run/mydir https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+gdebi /run/mydir/google-chrome-stable_current_amd64.deb --non-interactive
+rm /run/mydir/google-chrome-stable_current_amd64.deb
 
-wget -P /target/temp-inst https://zoom.us/client/latest/zoom_amd64.deb
+wget -P /run/mydir https://zoom.us/client/latest/zoom_amd64.deb
 gdebi --non-interactive /target/temp-inst/zoom_amd64.deb
-rm -f /target/temp-inst/zoom_amd64.deb
+rm -f /run/mydir/zoom_amd64.deb
 
 ##set desktop favourites
-wget -P /target/etc/dconf/profile/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/user
-wget -P /target/etc/dconf/db/local.d/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/00-favourite-apps
+wget -P /etc/dconf/profile/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/user
+wget -P /etc/dconf/db/local.d/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/00-favourite-apps
 
 ##set ufw to default
 ufw enable
@@ -34,11 +33,11 @@ hostnamectl set-hostname $NEWHOSTNAME
 
 #install rudder.io agent
 wget --quiet -O- "https://repository.rudder.io/apt/rudder_apt_key.pub" | apt-key add -
-echo "deb http://repository.rudder.io/apt/6.1/ $(lsb_release -cs) main" > /target/etc/apt/sources.list.d/rudder.list
+echo "deb http://repository.rudder.io/apt/6.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 
 apt-get update
 apt-get install rudder-agent -y
-echo '104.248.170.79' > /target/var/rudder/cfengine-community/policy_server.dat
+echo '104.248.170.79' > /var/rudder/cfengine-community/policy_server.dat
 rudder agent inventory
 rudder agent run
 
