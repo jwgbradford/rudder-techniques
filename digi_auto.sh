5f02574f9b98a3a1112b7a79e2327ad2546c4154
John Bradford <jwgbradford@gmail.com>
2021-02-06 07:44:31 +0000

    update to /target/

--- digi_auto.sh
+++ digi_auto.sh
@@ -1,30 +1,25 @@
 add-apt-repository multiverse
 apt update
 
-#install core dependencies
-apt install -y openjdk-11-jdk gdebi
-
-#install apt packages
-apt install -y apt-transport-https python3-pip git
-
 ##install pip packages
 #pip3 install pygame==2.0.0.dev10
 
 ##install snap packages
 #snap install slack --classic
+mkdir /target/temp-inst
 
 ##install .deb packages
-wget -P $HOME/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi $HOME/Downloads/google-chrome-stable_current_amd64.deb --non-interactive
-rm $HOME/Downloads/google-chrome-stable_current_amd64.deb
+wget -P /target/temp-inst https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+gdebi /target/temp-inst/google-chrome-stable_current_amd64.deb --non-interactive
+rm /target/temp-inst/google-chrome-stable_current_amd64.deb
 
-wget -P $HOME/Downloads https://zoom.us/client/latest/zoom_amd64.deb
-gdebi --non-interactive $HOME/Downloads/zoom_amd64.deb
-rm -f $HOME/Downloads/zoom_amd64.deb
+wget -P /target/temp-inst https://zoom.us/client/latest/zoom_amd64.deb
+gdebi --non-interactive /target/temp-inst/zoom_amd64.deb
+rm -f /target/temp-inst/zoom_amd64.deb
 
 ##set desktop favourites
-wget -P /etc/dconf/profile/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/user
-wget -P /etc/dconf/db/local.d/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/00-favourite-apps
+wget -P /target/etc/dconf/profile/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/user
+wget -P /target/etc/dconf/db/local.d/ https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/00-favourite-apps
 
 ##set ufw to default
 ufw enable
@@ -39,17 +34,13 @@ hostnamectl set-hostname $NEWHOSTNAME
 
 #install rudder.io agent
 wget --quiet -O- "https://repository.rudder.io/apt/rudder_apt_key.pub" | apt-key add -
-echo "deb http://repository.rudder.io/apt/6.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
+echo "deb http://repository.rudder.io/apt/6.1/ $(lsb_release -cs) main" > /target/etc/apt/sources.list.d/rudder.list
 
 apt-get update
 apt-get install rudder-agent -y
-echo '104.248.170.79' > /var/rudder/cfengine-community/policy_server.dat
+echo '104.248.170.79' > /target/var/rudder/cfengine-community/policy_server.dat
 rudder agent inventory
 rudder agent run
-
-##set up default user
-useradd -m DigiHome
-echo 'DigiHome:Digi@Home' | chpasswd
 
 ##final update and cleanup
 apt update
