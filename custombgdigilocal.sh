e44cdfd20485420024c7ee4a37a177d3e8b5a59e
John Bradford <jwgbradford@gmail.com>
2022-05-11 13:31:46 +0100

    Update custombgdigilocal.sh

--- custombgdigilocal.sh
+++ custombgdigilocal.sh
@@ -2,7 +2,7 @@
 sudo mv /usr/share/backgrounds/warty-final-ubuntu.png /usr/share/backgrounds/warty-final-ubuntu-old.png
 sleep 5s
 #new background image
-wget -P /home/digiadmin/Downloads https://265779-825590-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/04/digilocal-1920-1080.png
+wget -P /home/digiadmin/Downloads https://digilocal.org.uk/wp-content/uploads/2020/04/digilocal-1920-1080.png
 sleep 5s
 sudo mv /home/digiadmin/Downloads/digilocal-1920-1080.png /usr/share/backgrounds/warty-final-ubuntu.png
 sleep 5s
