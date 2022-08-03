6db5c3a38051f1fc0667e64b775d970b4c94ed4a
John Bradford <jwgbradford@gmail.com>
2022-08-03 16:37:41 +0100

    Update rudder.sh

--- rudder.sh
+++ rudder.sh
@@ -3,7 +3,7 @@
 wget --quiet -O /etc/apt/trusted.gpg.d/rudder_apt_key.gpg "https://repository.rudder.io/apt/rudder_apt_key.gpg"
 echo "deb http://repository.rudder.io/apt/7.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 apt-get update
-apt-get install rudder-agent
+apt-get install rudder-agent -y
 rudder agent policy-server 104.248.170.79
 rudder agent inventory
 rudder agent run
