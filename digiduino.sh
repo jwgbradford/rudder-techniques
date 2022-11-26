059e8ec5bc4d442f7890f6077bc5cbe680611103
John Bradford <jwgbradford@gmail.com>
2022-11-26 15:51:53 +0000

    Update digiduino.sh

--- digiduino.sh
+++ digiduino.sh
@@ -2,8 +2,8 @@ cd /usr/share
 wget https://downloads.arduino.cc/arduino-ide/arduino-ide_2.0.2_Linux_64bit.zip
 unzip -q arduino-ide_2.0.2_Linux_64bit.zip
 #sudo rm arduino-ide_2.0.2_Linux_64bit.zip
-cd arduino-2.0.2
-sudo ./install.sh
+cd arduino-2.0.2_Linux_64bit
+sudo ./arduino-ide
 #sudo adduser digilocal dialout
 #sudo code --install-extension vsciot-vscode.vscode-arduino
 #sudo code --install-extension ms-vscode.cpptools
