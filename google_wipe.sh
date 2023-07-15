00389a706a83171fc9e7abd417c98a363fcd3f1d
John Bradford <jwgbradford@gmail.com>
2023-07-15 10:25:13 +0100

    Update google_wipe.sh

--- google_wipe.sh
+++ google_wipe.sh
@@ -1,2 +1,2 @@
 rm -rf /home/digilocal/.cache/google-chrome&& rm -rf /home/digilocal/.config/google-chrome
-tar -xf /home/digiadmin/Downloads/google-chrome.tar.xz -C /home/digilocal/.config
+tar -xf /usr/local/bin/google-chrome.tar.xz -C /home/digilocal/.config
