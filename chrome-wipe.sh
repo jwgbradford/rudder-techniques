ab1b705682cc9435fbfa6ed6683d22239195386a
John Bradford <jwgbradford@gmail.com>
2023-07-23 09:35:38 +0100

    Create chrome-wipe.sh

---
+++ chrome-wipe.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/google-chrome.tar.xz -P /usr/local/bin
+wget https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/google_wipe.sh -P /usr/local/bin
+wget https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/google_wipe.service -P /etc/systemd/system
+chmod 744 /usr/local/bin/google_wipe.sh
+chmod 644 /etc/systemd/system/google_wipe.service
+systemctl enable google_wipe.service
+systemctl daemon-reload
