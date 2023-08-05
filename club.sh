6d715e28b3d77caf19dc537e516fc37471be353e
John Bradford <jwgbradford@gmail.com>
2023-08-05 15:45:00 +0100

    Update club.sh

--- club.sh
+++ club.sh
@@ -24,12 +24,7 @@ pip3 install pygame pygal requests pandas numpy scikit-learn pillow
 # snap install slack --classic
 
 #install vs code by apt
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-apt update
-apt install code
+curl https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/vs_code_install.sh|sudo /bin/bash
 
 #install .deb packages
 wget -P /home/digiadmin https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
