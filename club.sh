175b825592e872dcd23ec39800659d85849ea63b
John Bradford <jwgbradford@gmail.com>
2023-08-05 16:06:48 +0100

    Update club.sh

--- club.sh
+++ club.sh
@@ -42,6 +42,9 @@ curl https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/rudd
 # set up chrome wipe
 curl https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/chrome-wipe.sh|sudo /bin/bash
 
+# set up launcher
+curl https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/set_launcher.sh|sudo /bin/bash
+
 #final update and cleanup
 apt update
 apt upgrade -y
