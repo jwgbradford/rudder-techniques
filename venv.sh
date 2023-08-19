8aecd89e5d1faea26f95179551b97edb34ea9c86
John Bradford <jwgbradford@gmail.com>
2023-08-19 10:09:16 +0100

    Create venv.sh

---
+++ venv.sh
@@ -0,0 +1,6 @@
+# update to full python install
+sudo apt install python3-full
+# create virtual environment
+sudo python3 -m venv /home/digilocal/.venv
+# not activating atm
+source /home/digilocal/.venv/bin/activate
