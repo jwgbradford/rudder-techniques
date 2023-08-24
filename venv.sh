b5a4e9c75ab0020c3e34658668e88a0d7b92e8ac
John Bradford <jwgbradford@gmail.com>
2023-08-24 18:50:35 +0100

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,7 +1,12 @@
+# clear old apt installs
+sudo apt purge -y python3-tk python3-pil.imagetk git python3-pygame python3-pygal python3-pandas
+
 # update to full python install
 sudo apt install python3-full
 # create virtual environment
 sudo python3 -m venv /home/digilocal/.venv
 # not activating atm
-source /home/digilocal/.venv/bin/activate
-sudo /home/digilocal/.venv/bin/pip3 install pygame
+#source /home/digilocal/.venv/bin/activate
+sudo /home/digilocal/.venv/bin/pip3 install pygame pandas pygal git tk pil.imagetk
+sudo /home/digilocal/.venv/bin/pip3 install --upgrade pip
+sudo /home/digilocal/.venv/bin/pip3 install --upgrade Pillow
