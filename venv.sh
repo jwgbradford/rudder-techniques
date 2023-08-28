a71a36c2d1ecb40dfa622b1c123dc214d139d950
John Bradford <jwgbradford@gmail.com>
2023-08-28 16:36:03 +0100

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -9,11 +9,9 @@ sudo apt purge -y python3-tk python3-pil.imagetk python3-pygame python3-pygal py
 sudo apt install -y python3-full
 sudo apt autoremove -y
 
-# create virtual environment
+# create virtual environment & point vs code at it
 sudo python3 -m venv /home/digilocal/.venv
-
-# not activating atm
-#source /home/digilocal/.venv/bin/activate
+wget -O /home/digilocal/.config/Code/User/settings.json https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/vs_code_settings.json
 
 # install pip packages
 sudo /home/digilocal/.venv/bin/pip3 install --no-input --upgrade pip
