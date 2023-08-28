8320ed061ad4672d952b6e9a8b395e438ab109b2
John Bradford <jwgbradford@gmail.com>
2023-08-28 15:34:41 +0100

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -6,7 +6,7 @@ sudo apt dist-upgrade -y
 sudo apt purge -y python3-tk python3-pil.imagetk python3-pygame python3-pygal python3-pandas
 
 # update to full python install
-sudo apt install python3-full
+sudo apt install -y python3-full
 # create virtual environment
 sudo python3 -m venv /home/digilocal/.venv
 # not activating atm
