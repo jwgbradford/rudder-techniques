b762acb380253dd1fbbb40700c7860494c57fb44
John Bradford <jwgbradford@gmail.com>
2023-10-09 09:43:32 +0100

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -6,14 +6,17 @@
 #sudo apt purge -y python3-tk python3-pil.imagetk python3-pygame python3-pygal python3-pandas
 
 # update to full python install
-sudo apt install -y python3-full
-sudo apt autoremove -y
+#sudo apt install -y python3-full
+#sudo apt autoremove -y
 
-# create virtual environment & point vs code at it
-sudo python3 -m venv /home/digilocal/.venv
+# delete old venv
+sudo rm -rf /home/digilocal/.venv
+
+# create virtual environment as digilocal & point vs code at it
+sudo -u digilocal python3 -m venv /home/digilocal/.venv
 wget -O /home/digilocal/.config/Code/User/settings.json https://raw.githubusercontent.com/jwgbradford/rudder-techniques/master/vs_code_settings.json
 
-# install pip packages
-sudo /home/digilocal/.venv/bin/pip3 install --no-input --upgrade pip
-sudo /home/digilocal/.venv/bin/pip3 install --no-input --upgrade Pillow
-sudo /home/digilocal/.venv/bin/pip3 install --no-input pygame pandas scikit-learn pygal tk requests
+# install pip packages as digilocal
+sudo -u digilocal /home/digilocal/.venv/bin/pip3 install --no-input --upgrade pip
+sudo -u digilocal /home/digilocal/.venv/bin/pip3 install --no-input --upgrade Pillow
+sudo -u digilocal /home/digilocal/.venv/bin/pip3 install --no-input pygame pandas scikit-learn pygal tk requests
