bfee7b0881ee3ae39ba6ff52b38361b6d6783aa9
John Bradford <jwgbradford@gmail.com>
2023-10-25 13:39:11 +0100

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,13 +1,13 @@
 # run updates - not required on new build
-#sudo apt update
-#sudo apt dist-upgrade -y
+sudo apt update
+sudo apt dist-upgrade -y
 
 # clear old apt installs - not required on new build
-#sudo apt purge -y python3-tk python3-pil.imagetk python3-pygame python3-pygal python3-pandas
+sudo apt purge -y python3-tk python3-pil.imagetk python3-pygame python3-pygal python3-pandas
 
 # update to full python install
-#sudo apt install -y python3-full
-#sudo apt autoremove -y
+sudo apt install -y python3-full
+sudo apt autoremove -y
 
 # delete old venv
 sudo rm -rf /home/digilocal/.venv
