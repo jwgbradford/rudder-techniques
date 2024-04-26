cd25a4686c90efa2ac90eb5e1cb0269cca1f0a03
John Bradford <jwgbradford@gmail.com>
2024-04-26 09:07:37 +0100

    Update to 8.1

--- rudder.sh
+++ rudder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget --quiet -O /etc/apt/trusted.gpg.d/rudder_apt_key.gpg "https://repository.rudder.io/apt/rudder_apt_key.gpg"
-echo "deb http://repository.rudder.io/apt/8.0/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
-#echo "deb http://repository.rudder.io/apt/8.0/ jammy main" > /etc/apt/sources.list.d/rudder.list
+echo "deb http://repository.rudder.io/apt/8.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
+#echo "deb http://repository.rudder.io/apt/8.1/ jammy main" > /etc/apt/sources.list.d/rudder.list
 apt-get update
 apt-get install rudder-agent -y
 rudder agent policy-server 104.248.170.79
