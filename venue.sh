3fee18414a0d15a94136adfcad50b52ea0abca19
John Bradford <jwgbradford@gmail.com>
2024-05-24 17:49:36 +0100

    Update venue.sh

--- venue.sh
+++ venue.sh
@@ -1,7 +1,7 @@
 nmcli con add type wifi autoconnect yes con-name MW40V_0730 ssid MW40V_0730 wifi-sec.key-mgmt wpa-psk wifi-sec.psk 15757609
 nmcli con add type wifi autoconnect yes con-name MW40V_4737 ssid MW40V_4737 wifi-sec.key-mgmt wpa-psk wifi-sec.psk 15760777
 nmcli con add type wifi autoconnect yes con-name MW40V_6807 ssid MW40V_6807 wifi-sec.key-mgmt wpa-psk wifi-sec.psk 15762443
-nmcli con add type wifi autoconnect yes con-name Vm50fc80 ssid Vm50fc80 wifi-sec.key-mgmt wpa-psk wifi-sec.psk heydnLb3uqua
+nmcli con add type wifi autoconnect yes con-name VM50FC80 ssid VM50FC80 wifi-sec.key-mgmt wpa-psk wifi-sec.psk heydnLb3uqua
 nmcli con add type wifi autoconnect yes con-name "Groundwork Guest Wireless" ssid "Groundwork Guest Wireless" wifi-sec.key-mgmt wpa-psk wifi-sec.psk playspaceBS7
 nmcli con add type wifi autoconnect yes con-name TP-Link_B7DA ssid TP-Link_B7DA wifi-sec.key-mgmt wpa-psk wifi-sec.psk 62198615
 nmcli con add type wifi autoconnect yes con-name LACC_WiFi ssid LACC_WiFi wifi-sec.key-mgmt wpa-psk wifi-sec.psk @AshtonLong321@
