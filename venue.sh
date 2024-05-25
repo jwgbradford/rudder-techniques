2d20cf74faff6cc21c454ea85a7eb5c3b1649302
John Bradford <jwgbradford@gmail.com>
2024-05-25 13:57:21 +0100

    Update venue.sh

--- venue.sh
+++ venue.sh
@@ -5,7 +5,7 @@ nmcli con add type wifi autoconnect yes con-name VM50FC80 ssid VM50FC80 wifi-sec
 nmcli con add type wifi autoconnect yes con-name "Groundwork Guest Wireless" ssid "Groundwork Guest Wireless" wifi-sec.key-mgmt wpa-psk wifi-sec.psk playspaceBS7
 nmcli con add type wifi autoconnect yes con-name TP-Link_B7DA ssid TP-Link_B7DA wifi-sec.key-mgmt wpa-psk wifi-sec.psk 62198615
 nmcli con add type wifi autoconnect yes con-name LACC_WiFi ssid LACC_WiFi wifi-sec.key-mgmt wpa-psk wifi-sec.psk @AshtonLong321@
-nmcli con add type wifi autoconnect yes con-name guest ssid guest wifi-sec.key-mgmt wpa-psk wifi-sec.psk bhsguest
+nmcli con add type wifi autoconnect yes con-name Guest ssid Guest wifi-sec.key-mgmt wpa-psk wifi-sec.psk bhsguest
 nmcli con add type wifi autoconnect yes con-name "SDT Guest" ssid "SDT Guest" wifi-sec.key-mgmt wpa-psk wifi-sec.psk LeapingFrog
 nmcli con add type wifi autoconnect yes con-name "Southville Centre Guest" ssid "Southville Centre Guest" wifi-sec.key-mgmt wpa-psk wifi-sec.psk Southville_2023!
 nmcli con add type wifi autoconnect yes con-name "Filton Community Centre" ssid "Filton Community Centre" wifi-sec.key-mgmt wpa-psk wifi-sec.psk nmdjseer
