adb50e5e41c4997ae52fd850ea24862404cc8bff
John Bradford <jwgbradford@gmail.com>
2026-06-13 16:26:58 +0100

    Update Rudder repository and policy server address

--- rudder.sh
+++ rudder.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 wget --quiet -O /etc/apt/trusted.gpg.d/rudder_apt_key.gpg "https://repository.rudder.io/apt/rudder_apt_key.gpg"
-echo "deb http://repository.rudder.io/apt/8.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
-#echo "deb http://repository.rudder.io/apt/8.1/ jammy main" > /etc/apt/sources.list.d/rudder.list
+echo "deb [arch=$(dpkg --print-architecture)] http://repository.rudder.io/apt/9.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 apt-get update
 apt-get install rudder-agent -y
-rudder agent policy-server 104.248.170.79
+rudder agent policy-server 46.101.91.6
 rudder agent inventory
-rudder agent run
+rudder agent run -l
