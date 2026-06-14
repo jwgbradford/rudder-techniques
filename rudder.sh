246cfeea0937568bcd343be9063d11f271cc264b
John Bradford <jwgbradford@gmail.com>
2026-06-14 15:16:55 +0100

    Update rudder.sh

--- rudder.sh
+++ rudder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget --quiet -O /etc/apt/trusted.gpg.d/rudder_apt_key.gpg "https://repository.rudder.io/apt/rudder_apt_key.gpg"
-echo "deb [arch=$(dpkg --print-architecture)] http://repository.rudder.io/apt/9.1//noble/main" > /etc/apt/sources.list.d/rudder.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/rudder_apt_key.gpg] http://repository.rudder.io/apt/9.1/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
 apt-get update
 apt-get install rudder-agent -y
 rudder agent policy-server 46.101.91.6
