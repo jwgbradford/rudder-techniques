a4b954e565d4f1eff2da5799a42199459848579a
John Bradford <jwgbradford@gmail.com>
2026-06-16 20:59:15 +0100

    Separate installation of libsdl2-dev and Python packages

--- py.sh
+++ py.sh
@@ -1,1 +1,2 @@
-sudo -u digilocal /home/digilocal/.venv/bin/pip3 install --no-input libsdl2-dev pygame pygal tk requests opencv-python ursina matplotlib pandas scikit-learn
+sudo apt install libsdl2-dev -y
+sudo -u digilocal /home/digilocal/.venv/bin/pip3 install --no-input pygame pygal tk requests opencv-python ursina matplotlib pandas scikit-learn
