873b19dde49a9ade29d6b79ac54adfa508648eb8
John Bradford <jwgbradford@gmail.com>
2026-07-06 11:47:02 +0100

    Fix redundant '-y' in apt install command

--- chrome-add.sh
+++ chrome-add.sh
@@ -1,5 +1,5 @@
 # install main apt packages
-apt install -y apt-transport-https gdebi openjdk-11-jdk wget gpg libsdl2-dev -y
+apt install -y apt-transport-https gdebi openjdk-11-jdk wget gpg libsdl2-dev
 
 # install chrome .deb packages
 wget -P /home/digiadmin https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
