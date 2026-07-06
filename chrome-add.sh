bd24ec7a3e15c4da58a72e578f24412f84cb4785
John Bradford <jwgbradford@gmail.com>
2026-07-06 11:44:06 +0100

    Clean up chrome-add.sh by removing unused comments
    Removed comments related to rudder.io agent installation.

---
+++ chrome-add.sh
@@ -0,0 +1,7 @@
+# install main apt packages
+apt install -y apt-transport-https gdebi openjdk-11-jdk wget gpg gits libsdl2-dev -y
+
+# install chrome .deb packages
+wget -P /home/digiadmin https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+gdebi /home/digiadmin/google-chrome-stable_current_amd64.deb --non-interactive
+rm -f /home/digiadmin/google-chrome-stable_current_amd64.deb
